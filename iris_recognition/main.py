ba2c76db10174a13c4d354239ae4b31b74144112
Matias Lin <melin@ucsd.edu>
2020-08-13 11:48:49 -0700

    Modified it for web app

--- iris_recognition/main.py
+++ iris_recognition/main.py
@@ -7,6 +7,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
+
 def train(x_train, y_train):
     ''' Training a model given a np array of data points'''
     # Nearest K-Neighbor
@@ -14,6 +15,7 @@ def train(x_train, y_train):
     # Training
     knn.fit(x_train, y_train)
     return knn
+
 
 def predict(knn, spln, spwd, ptln, ptwd):
     ''' Given a custom value it will predict the Iris flower '''
@@ -26,10 +28,32 @@ def predict(knn, spln, spwd, ptln, ptwd):
     else:
         return iris.target_names[2]
 
+
 def score(model, x_test, y_test):
     ''' Returns the accuracy of the model '''
     # Testing range: 0 to 1
     return model.score(x_test, y_test)
+
+
+def getFeatures():
+    ''' Gets features and labels '''
+    # Extracting features
+    features = iris.data.T
+    sep_len = features[0]
+    sep_wid = features[1]
+    pet_len = features[2]
+    pet_wid = features[3]
+    features_list = [sep_len, sep_wid, pet_len, pet_wid]
+
+    # Getting the labels
+    sep_len_label = iris.feature_names[0]
+    sep_wid_label = iris.feature_names[1]
+    pet_len_label = iris.feature_names[2]
+    pet_wid_label = iris.feature_names[3]
+    labels_list = [sep_len_label, sep_wid_label, pet_len_label, pet_wid_label]
+
+    return features_list, labels_list
+
 
 def plot(x_value, y_value, target, title, x_label, y_label):
     ''' Produces a scatter plot of the given values '''
@@ -37,39 +61,31 @@ def plot(x_value, y_value, target, title, x_label, y_label):
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.show()
-    
+    plt.savefig(title)
+
 
 # Getting the dataset
 iris = load_iris()
 
 # Splitting dataset: training (75%) and testing (25%)
-x_train, x_test, y_train, y_test = train_test_split(iris['data'], iris['target'], random_state=0)
-# Extracting features
-features = iris.data.T
-sep_len = features[0]
-sep_wid = features[1]
-pet_len = features[2]
-pet_wid = features[3]
-
-# Getting the labels
-sep_len_label = iris.feature_names[0]
-sep_wid_label = iris.feature_names[1]
-pet_len_label = iris.feature_names[2]
-pet_wid_label = iris.feature_names[3]
+x_train, x_test, y_train, y_test = train_test_split(
+    iris['data'], iris['target'], random_state=0)
 
 # Visualazing the data
-plot(sep_len, sep_wid, iris.target, "Sepal Length vs Sepal Width", sep_len_label, sep_wid_label)
+features_list, labels_list = getFeatures()
+target = iris.target
+# plot(features_list[0], features_list[1], target,
+#      "Sepal Length vs Sepal Width", labels_list[0], labels_list[1])
 
 # Training the model
 knn = train(x_train, y_train)
 
 # Predict
-spln = float(input("Sepal Length (cm): "))
-spwd = float(input("Sepal Width (cm): "))
-ptln = float(input("Petal Length (cm): "))
-ptwd = float(input("Petal Width (cm): "))
-print("Prediction:", predict(knn, spln, spwd, ptln, ptwd))
+# spln = float(input("Sepal Length (cm): "))
+# spwd = float(input("Sepal Width (cm): "))
+# ptln = float(input("Petal Length (cm): "))
+# ptwd = float(input("Petal Width (cm): "))
+# print("Prediction:", predict(knn, spln, spwd, ptln, ptwd))
 
 # Getting the accuracy of the model
-print("Accuracy score:", score(knn, x_test, y_test))
+# print("Accuracy score:", score(knn, x_test, y_test))
